90113ad9920ce7d2bc52a24ffb95ff610ec70323
iamHAMIDREZA1 <ataei1564@gmail.com>
2023-07-16 22:27:09 +0330

    detail,result,vote in view , add admin

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,15 @@
+from django import http
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at the results of question %s." % question_id)
+
+def result(request, question_id):
+    return HttpResponse("You're looking at the results of the question %s" % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id)
