8f31523bf97cc0ecdb35e514ef59a037696afc0f
HamidReza <ataei1564@gmail.com>
2023-07-19 19:00:03 +0330

    writing test and solving problem of was_recently_published in future questions.

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from datetime import timedelta
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+class QuestionModelTests(TestCase):
+    def test_Was_published_recently_with_fture_question(self):
+        """
+        was_published_recently returns False for questions whose\
+        pub_date is in the future
+        """
+        
+        future_time = timezone.now()+timedelta(days=33)
+        future_question = Question.objects.create(
+            question_text="future_question", pub_date=future_time)
+        msg = "questions create in future time are NOT recently published."
+        self.assertIs(future_question.was_published_recently(), False, msg)
