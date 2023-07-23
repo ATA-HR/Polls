393ce6de40eb43c2aa89e49a956a05da6cf03d43
HamidReza <ataei1564@gmail.com>
2023-07-23 21:53:59 +0330

    add default parameter

--- polls/tests.py
+++ polls/tests.py
@@ -5,40 +5,42 @@ from django.utils import timezone
 
 from .models import Question
 
+def create_question(question_text, days, hours=0, minutes=0, seconds=0):
+    """Create questions"""
+
+    time = timezone.now() + timedelta(days=days, hours=hours,
+        minutes=minutes, seconds=seconds)
+    question = Question.objects.create(question_text=question_text, pub_date=time)
+    return question
+
 class QuestionModelTests(TestCase):
     def test_Was_published_recently_with_fture_question(self):
         """
-        was_published_recently returns False for questions whose\
+        Was_published_recently returns False for questions whose\
         pub_date is in the future
         """
         
-        future_time = timezone.now()+timedelta(days=33) 
-        future_question = Question.objects.create(
-            question_text="future_question", pub_date=future_time)
+        future_question = create_question("future_question", 30)
         msg = "questions create in future time was NOT recently published."
         self.assertIs(future_question.was_published_recently(), False, msg)
         
     def test_was_published_recently_for_old_question(self):
         """
-        was_published_recently returns False for questions whose\
+        Was_published_recently returns False for questions whose\
         pub_date is in the past
         """
         
-        old_time = timezone.now() - timedelta(days=1, seconds=1)
-        old_question = Question.objects.create(
-            question_text="old_question", pub_date=old_time)
+        old_question = create_question("old_question", -30)
         msg = "questions create in past was NOT recently published."
         self.assertIs(old_question.was_published_recently(), False, msg)
         
     def test_was_published_recently_for_recent_question(self):
         """
-        was_published_recently returns True for questions whose\
+        Was_published_recently returns True for questions whose\
         pub_date is recently
         """
         
-        time = timezone.now() - timedelta(hours=23,minutes=59, seconds=59)
-        question = Question.objects.create(
-            question_text="question", pub_date=time)
-        msg = "questions create recently published recently."
+        question = create_question("question", 0, -22, -59, -59)
+        msg = "questions created recently published recently."
         self.assertIs(question.was_published_recently(), True, msg)
         
